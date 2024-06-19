612fb0e392ef810c2c456b4dcc362a1a2440d5d4
SirFateAndRage <milanicristian94@gmail.com>
2024-06-19 18:10:13 -0300

    [A] Map [A] VPS and instruction popups

---
+++ Assets/Scripts/Slider/TextSliderInitializer.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using TMPro;
+using UnityEngine;
+
+namespace Sliders
+{
+
+    public class TextSliderInitializer : SliderInitializizer
+    {
+        [SerializeField] private TMP_Text _text;
+
+        [SerializeField] private GameObject _background;
+        [SerializeField] private float _fadeDuration;
+
+        private void Awake()
+        {
+            Color color = _text.color;
+            color.a = 0;
+            _text.color = color;
+            _background.SetActive(false);
+        }
+
+        public override void OnClose()
+        {
+            StartCoroutine(FadeTextToZeroAlpha());
+        }
+
+        public override void OnInit()
+        {
+            _background.SetActive(true);
+            StartCoroutine(FadeTextToFullAlpha());
+        }
+
+        IEnumerator FadeTextToFullAlpha()
+        {
+            float elapsedTime = 0;
+            Color color = _text.color;
+
+            while (elapsedTime < _fadeDuration)
+            {
+                elapsedTime += Time.deltaTime;
+                color.a = Mathf.Clamp01(elapsedTime / _fadeDuration);
+                _text.color = color;
+                yield return null;
+            }
+
+            color.a = 1;
+            _text.color = color;
+        }
+
+
+
+        IEnumerator FadeTextToZeroAlpha()
+        {
+            float elapsedTime = 0;
+            Color color = _text.color;
+
+            while (elapsedTime < _fadeDuration)
+            {
+                elapsedTime += Time.deltaTime;
+                color.a = Mathf.Clamp01(1 - (elapsedTime / _fadeDuration));
+                _text.color = color;
+                yield return null;
+            }
+
+            color.a = 0;
+            _text.color = color;
+            _background.SetActive(false);
+        }
+    }
+}
