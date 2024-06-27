f147b728735eb6781e69b9c9028ef30c78374c12
SirFateAndRage <milanicristian94@gmail.com>
2024-06-27 14:13:20 -0300

    [F] Tutorial

--- Assets/Scripts/Slider/TextSliderInitializer.cs
+++ Assets/Scripts/Slider/TextSliderInitializer.cs
@@ -2,72 +2,135 @@
 using System.Collections;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Sliders
 {
     public class TextSliderInitializer : SliderInitializizer
     {
         [SerializeField]
-        private TMP_Text _text;
+        private TMP_Text[] _texts;
 
         [SerializeField]
-        private GameObject _background;
+        private Image[] _backgrounds;
 
         [SerializeField]
         private float _fadeDuration;
 
+        [SerializeField]
+        private GameObject _container;
+
         private void Awake()
         {
-            Color color = _text.color;
-            color.a = 0;
-            _text.color = color;
-            _background.SetActive(false);
+            foreach (var text in _texts)
+            {
+                Color color = text.color;
+                color.a = 0;
+                text.color = color;
+            }
+
+            foreach (var background in _backgrounds)
+            {
+                Color color = background.color;
+                color.a = 0;
+                background.color = color;
+                //background.gameObject.SetActive(false);
+            }
+            _container.SetActive(false);
         }
 
         public override void OnClose()
         {
-            StartCoroutine(FadeTextToZeroAlpha());
+            StartCoroutine(FadeTextsAndBackgroundsToZeroAlpha());
         }
 
         public override void OnInit()
         {
-            _background.SetActive(true);
-            StartCoroutine(FadeTextToFullAlpha());
+            _container.SetActive(true);
+            StartCoroutine(FadeTextsAndBackgroundsToFullAlpha());
         }
 
-        IEnumerator FadeTextToFullAlpha()
+        private IEnumerator FadeTextsAndBackgroundsToFullAlpha()
         {
             float elapsedTime = 0;
-            Color color = _text.color;
 
             while (elapsedTime < _fadeDuration)
             {
                 elapsedTime += Time.deltaTime;
-                color.a = Mathf.Clamp01(elapsedTime / _fadeDuration);
-                _text.color = color;
+                float alpha = Mathf.Clamp01(elapsedTime / _fadeDuration);
+
+                foreach (var text in _texts)
+                {
+                    Color color = text.color;
+                    color.a = alpha;
+                    text.color = color;
+                }
+
+                foreach (var background in _backgrounds)
+                {
+                    Color color = background.color;
+                    color.a = alpha;
+                    background.color = color;
+                }
+
                 yield return null;
             }
 
-            color.a = 1;
-            _text.color = color;
+            foreach (var text in _texts)
+            {
+                Color color = text.color;
+                color.a = 1;
+                text.color = color;
+            }
+
+            foreach (var background in _backgrounds)
+            {
+                Color color = background.color;
+                color.a = 1;
+                background.color = color;
+            }
         }
 
-        IEnumerator FadeTextToZeroAlpha()
+        private IEnumerator FadeTextsAndBackgroundsToZeroAlpha()
         {
             float elapsedTime = 0;
-            Color color = _text.color;
 
             while (elapsedTime < _fadeDuration)
             {
                 elapsedTime += Time.deltaTime;
-                color.a = Mathf.Clamp01(1 - (elapsedTime / _fadeDuration));
-                _text.color = color;
+                float alpha = Mathf.Clamp01(1 - (elapsedTime / _fadeDuration));
+
+                foreach (var text in _texts)
+                {
+                    Color color = text.color;
+                    color.a = alpha;
+                    text.color = color;
+                }
+
+                foreach (var background in _backgrounds)
+                {
+                    Color color = background.color;
+                    color.a = alpha;
+                    background.color = color;
+                }
+
                 yield return null;
             }
 
-            color.a = 0;
-            _text.color = color;
-            _background.SetActive(false);
+            foreach (var text in _texts)
+            {
+                Color color = text.color;
+                color.a = 0;
+                text.color = color;
+            }
+
+            foreach (var background in _backgrounds)
+            {
+                Color color = background.color;
+                color.a = 0;
+                background.color = color;
+            }
+            _container.SetActive(false);
         }
     }
 }
